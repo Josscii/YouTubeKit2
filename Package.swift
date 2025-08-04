c6360e16c47edc0190d4f9a4d8d97373d94a2a7b
Josscii <jossciiweiyi@gmail.com>
2025-08-04 23:12:44 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "YouTubeKit",
+    name: "YouTubeKit2",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "YouTubeKit",
-            targets: ["YouTubeKit"]),
+            name: "YouTubeKit2",
+            targets: ["YouTubeKit2"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YouTubeKit",
+            name: "YouTubeKit2",
             dependencies: []/*,
             swiftSettings: [.unsafeFlags(["-Xfrontend", "-strict-concurrency=complete"])]*/),
         .testTarget(
             name: "YouTubeKitTests",
-            dependencies: ["YouTubeKit"]),
+            dependencies: ["YouTubeKit2"]),
     ]
 )
